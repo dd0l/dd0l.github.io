ad70d1e1d5a3668de7152f623ceb580da2d4872e
ain-lee <flfk97@naver.com>
2025-01-05 23:50:29 +0900

    블로그 포스팅
    1. html이란?
    2. visual studio 설치방법
    3. visual studio 확장프로그램 추천(수정중)

--- page/html-basic/html-common.js
+++ page/html-basic/html-common.js
@@ -1,3 +1,21 @@
+const body = document.querySelector('body')
+// 공통 lnb 추가
+let lnb = document.createElement('nav');
+lnb.setAttribute('id','lnb');
+let navHtml =`
+        <h2>HTML</h2>
+        <ul class="lnb-list">
+            <li><a href="whats-up-html.html">HTML 이란?</a></li>
+            <li><a href="vscode-install.html">Visual Studio 설치방법</a></li>
+            <li><a href="vscode-extends.html">Visual Studio 확장프로그램 추천</a></li>
+            <li><a href="#">< Tag > 종류</a></li>
+            <li><a href="#">< Tag > 사용법</a></li>
+        </ul>
+    `;
+
+body.prepend(lnb);
+lnb.innerHTML = navHtml;
+
 // 공통 header 추가
 let header = document.createElement('header');
 header.setAttribute('id','gnb');
@@ -10,22 +28,16 @@ let headerHtml =`
         </ul>
         <!-- serch 추가예정 -->
     `;
-const body = document.querySelector('body')
     body.prepend(header);
     header.innerHTML = headerHtml;
 
+// 동일한 href에 클래스 on 추가
+let url = window.location.pathname
+let aHref = document.querySelectorAll('.lnb-list a')
+aHref.forEach(a => {
+    a.classList.remove('on')
+    if(url == `/page/html-basic/${a.getAttribute('href')}`){
+        a.classList.add('on')
+    }
 
-
-// 공통 lnb 추가
-let lnb = document.createElement('nav');
-lnb.setAttribute('id','lnb');
-let navHtml =`
-        <h2>HTML</h2>
-        <ul class="lnb-list">
-            <li><a href="#">HTML 소개</a></li>
-            <li><a href="#">HTML 기초(1)</a></li>
-        </ul>
-    `;
-
-body.prepend(lnb);
-lnb.innerHTML = navHtml;
+});
